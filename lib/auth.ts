ebbfcee026e352ead4bedfa53824196704aeb0e3
Olawuni Olukunle <olawuni50@gmail.com>
2023-12-31 07:07:45 +0100

    google client

---
+++ lib/auth.ts
@@ -0,0 +1,64 @@
+import {NextAuthOptions} from 'next-auth'
+import GitHub from 'next-auth/providers/github'
+import Facebook from 'next-auth/providers/facebook'
+import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import {MongoClient} from 'mongodb'
+import {MongoDBAdapter} from "@auth/mongodb-adapter"
+import clientPromise from './mongodb'
+
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+          }),
+          CredentialsProvider({
+            name: 'email and password',
+            credentials: {
+                email: {
+                    label: "Email",
+                    placeholder:'email@email.com',
+                    type:'email'
+                },
+                userName: {
+                    label: "User Name",
+                    placeholder:'user name',
+                    type:'string'
+                },
+                password:{
+                    label:'Password',
+                    placeholder:'Enter password',
+                    type:'password'
+                }
+            },
+
+            async authorize(credentials, req){
+                if(!credentials?.email || !credentials?.password){
+                    throw new Error("Please provide valid credentials")
+                }
+
+                const client = await MongoClient.connect(process.env.MONGODB_URI as string)
+
+                const db = client.db();
+                const user = await db.collection('users').findOne({email:credentials.email})
+
+                if(user) {
+                    return user as any;
+                }else {
+                    return null;
+                }
+
+            }
+
+          })
+    ],
+    session: {
+        strategy: 'jwt',
+    },
+    adapter: MongoDBAdapter(clientPromise),
+    debug:process.env.NODE_ENV === 'development',
+    secret: process.env.NEXTAUTH_SECRET
+    // pages:{}
+}
---
+++ lib/mongodb.ts
@@ -0,0 +1,28 @@
+import {MongoClient} from 'mongodb';
+
+declare global{
+    var _mongoClientPromise: Promise<MongoClient>
+}
+
+
+let client;
+let clientPromise: any;
+
+if(!process.env.MONGODB_URI) {
+    throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
+}
+
+const uri = process.env.MONGODB_URI
+
+if(process.env.NODE_ENV === 'development'){
+    if(!global._mongoClientPromise){
+        client = new MongoClient(uri);
+        global._mongoClientPromise = client.connect();
+    }
+    clientPromise = global._mongoClientPromise
+}else{
+    client = new MongoClient(uri);
+    clientPromise = client.connect();
+}
+
+export default clientPromise
